c6528bca828dc97bd1ff4b94be15782f8222864d
Haiqel <34855234+Acerizm@users.noreply.github.com>
2023-09-16 18:11:35 +0800

    add routes and create home page

---
+++ front-end/peer-prep/src/components/homePage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div id="Home" style={{}}>
+            Hello World
+        </div>
+    )
+}
+
+export default HomePage;
