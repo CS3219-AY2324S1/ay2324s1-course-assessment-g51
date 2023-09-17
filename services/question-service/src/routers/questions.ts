b5f4101b272cba8592c2568289a102dc26a8bef6
Hui Zhuan <huizhuansam@hotmail.com>
2023-09-17 17:28:26 +0800

    add question routes

---
+++ services/question-service/src/routers/questions.ts
@@ -0,0 +1,38 @@
+import express from "express";
+
+import Question from "../models/question";
+
+const router = express.Router();
+
+router.post("/questions", async (req, res) => {
+  try {
+    const question = new Question({
+      ...req.body,
+    });
+    await question.save();
+    res.status(201).send(question);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// route parameters:
+// GET /api/questions?q=query+string
+router.get("/questions", async (req, res) => {
+  // TODO
+});
+
+router.get("/questions/:id", async (req, res) => {
+  try {
+    const question = await Question.findById(req.params.id);
+    if (!question) {
+      res.status(404).send();
+      return;
+    }
+    res.send(question);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+export default router;
