b0b2eb22d5ac927bd79931db8653bb2601c74b21
Zenith <zenithyap88@gmail.com>
2023-09-21 15:49:30 +0800

    Update UI for question creator (#12)

--- front-end/peer-prep/src/components/QuestionsPage/QuestionCreator/index.tsx
+++ front-end/peer-prep/src/components/QuestionsPage/QuestionCreator/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 // import inline styles
-import { questionCreatorContainerStyle } from "./styles";
+import { questionCreatorContainerStyle, questionCreatorViewStyle, labelContainerStyle, labelStyle, descriptionStyle, buttonStyle } from "./styles";
 
 const QuestionCreator = () => {
     return (
         <div style={questionCreatorContainerStyle}>
-
+            <div style = {questionCreatorViewStyle}>
+                <div style = {labelContainerStyle}>
+                    <div style={labelStyle}>id</div>
+                    <div style={labelStyle}>title</div>
+                    <div style={labelStyle}>complexity</div>
+                    <div style={labelStyle}>categories</div>
+                    <div style={descriptionStyle}>description</div>
+                    <button style={buttonStyle}>test</button>
+                </div>
+            </div>
         </div>
     )
 }
--- front-end/peer-prep/src/components/QuestionsPage/QuestionCreator/styles.tsx
+++ front-end/peer-prep/src/components/QuestionsPage/QuestionCreator/styles.tsx
@@ -1,4 +1,48 @@
 
 export const questionCreatorContainerStyle = {
-    border: "2px dotted red"
+    border: "2px dotted red",
+    justifyItems: "center",
+    alignItems: "center",
+    display: "grid"
+}
+
+export const questionCreatorViewStyle = {
+    display: "grid",
+    backgroundColor: "pink",
+    justifyItems: "center",
+    alignItems: "center",
+    height: "80%",
+    width: "90%",
+    borderRadius: "3%"
+}
+
+export const labelContainerStyle = {
+    height: "60%",
+    width: "100%",
+    display: "grid",
+    alignItems: "center",
+    justifyItems: "center",
+    gridRowGap: "5%"
+}
+
+export const labelStyle = {
+    backgroundColor: "white",
+    height: "100%",
+    width: "80%",
+    borderRadius: "10px",
+    alignItems: "center"
+}
+
+export const descriptionStyle = {
+    backgroundColor: "white",
+    height: "100%",
+    width: "80%",
+    gridRow: "span 4",
+    borderRadius: "10px",
+    alignItems: "center"
+}
+
+export const buttonStyle = {
+    justifySelf: "end",
+    marginRight: "10%"
 }
