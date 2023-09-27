de077cb02eda2e05a6b7281f15c2b029ad13bcd4
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-09-28 02:13:18 +0800

    Add CRUD api for profile service Add utils to make response message be consistent

---
+++ services/profile-service/src/controller/user-controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { ResponseUtil } from "../../utils/Response";
+import { User } from "../entity/User";
+import { AppDataSource } from "../data-source";
+
+export class UserController {
+	// async getAuthors(req: Request, res: Response): Promise<Response> {
+	//   const authors = await AppDataSource.getRepository(User).find();
+	//   return ResponseUtil.sendResponse(res, authors, 200);
+	// }
+
+	async getUser(req: Request, res: Response): Promise<Response> {
+		const { username } = req.params;
+		const user = await AppDataSource.getRepository(User).findOneBy({
+			username: username,
+		});
+		if (!user) {
+			return ResponseUtil.sendError(res, "User not found", 404);
+		}
+		return ResponseUtil.sendResponse(res, user, 200);
+	}
+
+	async createUser(req: Request, res: Response, next): Promise<Response> {
+		const userData = req.body;
+		const repo = AppDataSource.getRepository(User);
+		const user = repo.create(userData);
+		await repo.save(user);
+		return ResponseUtil.sendResponse(res, user, 201);
+	}
+
+	async updateUser(req: Request, res: Response): Promise<Response> {
+		const { username } = req.params;
+		const userData = req.body;
+		const repo = AppDataSource.getRepository(User);
+		const user = await repo.findOneBy({
+			username: username,
+		});
+		if (!user) {
+			return ResponseUtil.sendError(res, "user not found", 404);
+		}
+		repo.merge(user, userData);
+		await repo.save(user);
+		return ResponseUtil.sendResponse(res, user, 201);
+	}
+
+	async deleteUser(req: Request, res: Response): Promise<Response> {
+		const { username } = req.params;
+		const repo = AppDataSource.getRepository(User);
+		const user = await repo.findOneBy({
+			username: username,
+		});
+		if (!user) {
+			return ResponseUtil.sendError(res, "user not found", 404);
+		}
+		await repo.remove(user);
+		return ResponseUtil.sendResponse(res, null);
+	}
+}
