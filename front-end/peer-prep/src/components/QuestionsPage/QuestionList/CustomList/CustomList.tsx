a20237a9807468beaa8e217f9d5ae012664bc3cd
Edward Mualim <56242167+Edfernape@users.noreply.github.com>
2023-09-28 00:15:15 +0800

    Update question list (#14)
    * Add Material UI to packages
    
    * Add local database to redux
    
    * Add QuestionList

---
+++ front-end/peer-prep/src/components/QuestionsPage/QuestionList/CustomList/CustomList.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {List, ListItem} from "@mui/material";
+// import ListItem from "../ListItem/ListItem";
+import { Tooltip, Chip, IconButton, Stack, Divider } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from '@mui/icons-material/Edit';
+import localDatabase from "../../../redux/localDatabase.json";
+
+const CustomList = () => {
+    const [questions] = React.useState(localDatabase);
+
+    return(
+        <List sx={{
+            width: '100%',
+            maxWidth: '100%',
+            position: 'relative',
+            overflow: 'auto',
+            maxHeight: 800
+          }}>
+            {questions.map((question) => (
+                <ListItem>
+                    <Stack direction="column" sx={{width: '100%'}} style={{border: '1px black solid'}}>
+                        <Stack direction="row" justifyContent="space-between">
+                            <Stack direction="row" alignItems="center">
+                                {question.Id} {question.Title}
+                                <Chip label={question.Complexity}></Chip>
+                            </Stack>
+                            <Stack direction="row" justifyContent="flex-end">
+                                <Tooltip title="Edit">
+                                    <IconButton><EditIcon /></IconButton>
+                                </Tooltip>
+                                <Tooltip title="Delete">
+                                    <IconButton><DeleteIcon /></IconButton>
+                                </Tooltip>
+                            </Stack>
+                        </Stack>
+                        <Stack direction="row" spacing={2}>
+                            {question.Categories.map((category) => <Chip label={category}></Chip>)}
+                        </Stack>
+                    </Stack>
+                </ListItem>
+            ))}
+        </List>
+    );
+}
+
+export default CustomList;
--- front-end/peer-prep/src/components/QuestionsPage/QuestionList/index.tsx
+++ front-end/peer-prep/src/components/QuestionsPage/QuestionList/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import CustomList from "./CustomList/CustomList";
 
 // import inline styles
 import { questionListContainerStyle } from "./styles";
-
+import { Button, Stack, Typography, Divider} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 const QuestionList = () => {
     return (
         <div style={questionListContainerStyle}>
-
+            <Stack divider={<Divider orientation="horizontal" />}>
+                <Typography variant="h5">Questions</Typography>
+                <Button variant="outlined">
+                    <AddIcon></AddIcon>Add a Question
+                </Button>
+                <CustomList />
+            </Stack>
         </div>
     )
 }
