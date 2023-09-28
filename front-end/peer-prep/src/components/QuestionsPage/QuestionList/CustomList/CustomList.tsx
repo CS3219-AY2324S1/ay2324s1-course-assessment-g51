0a711e36ecc0c2c8612661c327f364c54d0471b7
Haiqel <acehql3@gmail.com>
2023-09-28 11:17:23 +0800

    update styling of stack UI components

--- front-end/peer-prep/src/components/QuestionsPage/QuestionList/CustomList/CustomList.tsx
+++ front-end/peer-prep/src/components/QuestionsPage/QuestionList/CustomList/CustomList.tsx
@@ -5,37 +5,32 @@ import { Tooltip, Chip, IconButton, Stack, Divider } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from '@mui/icons-material/Edit';
 import localDatabase from "../../../redux/localDatabase.json";
+import * as Style from "./styles"
 
 const CustomList = () => {
     const [questions] = React.useState(localDatabase);
 
     return(
-        <List sx={{
-            width: '100%',
-            maxWidth: '100%',
-            position: 'relative',
-            overflow: 'auto',
-            maxHeight: 500,
-          }}>
+        <List sx={Style.listStyle}>
             {questions.map((question) => (
                 <ListItem>
-                    <Stack direction="column" sx={{width: '100%'}} style={{border: '1px black solid'}}>
+                    <Stack direction="column" sx={Style.stackContainerStyle}>
                         <Stack direction="row" justifyContent="space-between">
-                            <Stack direction="row" alignItems="center">
-                                {question.Id} {question.Title}
-                                <Chip label={question.Complexity}></Chip>
+                            <Stack direction="row" alignItems="center" sx={Style.questionHeadingsStyle}>
+                                {question.Id}. {question.Title}
+                                <Chip label={question.Complexity} sx={Style.difficultyChipStyle}></Chip>
                             </Stack>
                             <Stack direction="row" justifyContent="flex-end">
                                 <Tooltip title="Edit">
-                                    <IconButton><EditIcon /></IconButton>
+                                    <IconButton sx={Style.iconButtonStyle}><EditIcon /></IconButton>
                                 </Tooltip>
                                 <Tooltip title="Delete">
-                                    <IconButton><DeleteIcon /></IconButton>
+                                    <IconButton style={Style.iconButtonStyle}><DeleteIcon /></IconButton>
                                 </Tooltip>
                             </Stack>
                         </Stack>
-                        <Stack direction="row" spacing={2}>
-                            {question.Categories.map((category) => <Chip label={category}></Chip>)}
+                        <Stack direction="row" spacing={2} sx={{marginLeft: "5%"}}>
+                            {question.Categories.map((category) => <Chip label={category} sx={Style.categoryChipStyle}></Chip>)}
                         </Stack>
                     </Stack>
                 </ListItem>
---
+++ front-end/peer-prep/src/components/QuestionsPage/QuestionList/CustomList/styles.tsx
@@ -0,0 +1,41 @@
+export const listStyle = {
+    width: '100%',
+    maxWidth: '100%',
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 500,
+}
+
+export const stackContainerStyle = {
+    width: "100%",
+    borderRadius: "15px",
+    backgroundColor: "#1B2735",
+    // Stack height can only use px -> MUI
+    height: "110px",
+}
+
+export const questionHeadingsStyle = {
+    color: "white",
+    fontFamily: "arial",
+    fontSize: "20px",
+    marginLeft: "5%"
+}
+
+export const iconButtonStyle = {
+    color: "white"
+}
+
+export const categoryChipStyle = {
+    color: "white",
+    fontFamily: "arial",
+    backgroundColor: "#131B25",
+    fontSize: "15px"
+}
+
+export const difficultyChipStyle = {
+    color: "#90EE90",
+    fontFamily: "arial",
+    backgroundColor: "#131B25",
+    fontSize: "15px",
+    marginLeft: "20px"
+}
