7a54090e31c5bbba1a6183fd4eaee893274b81b1
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-10-01 05:20:33 +0800

    Add dockerfile and docker-compose for profile service

--- services/profile-service/src/controller/user-controller.ts
+++ services/profile-service/src/controller/user-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { ResponseUtil } from "../../utils/Response";
+import { ResponseUtil } from "../utils/Response";
 import { User } from "../entity/User";
 import { AppDataSource } from "../data-source";
 
@@ -23,6 +23,27 @@ export class UserController {
 	async createUser(req: Request, res: Response, next): Promise<Response> {
 		const userData = req.body;
 		const repo = AppDataSource.getRepository(User);
+
+		//check for duplicate username
+		const checkUserName = await AppDataSource.getRepository(User).findOneBy(
+			{
+				username: userData.username,
+			}
+		);
+
+		if (checkUserName) {
+			return ResponseUtil.sendError(res, "Username already used", 404);
+		}
+
+		//check for duplicate email
+		const checkEmail = await AppDataSource.getRepository(User).findOneBy({
+			username: userData.username,
+		});
+
+		if (checkEmail) {
+			return ResponseUtil.sendError(res, "Email already used", 404);
+		}
+
 		const user = repo.create(userData);
 		await repo.save(user);
 		return ResponseUtil.sendResponse(res, user, 201);
