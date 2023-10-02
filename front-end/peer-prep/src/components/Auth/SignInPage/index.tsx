f3871c3a4952cbc07782a8307b14bb2bd763ed9f
Haiqel <34855234+Acerizm@users.noreply.github.com>
2023-10-02 22:18:20 +0800

    Add styling to Sign In component

--- front-end/peer-prep/src/components/Auth/SignInPage/index.tsx
+++ front-end/peer-prep/src/components/Auth/SignInPage/index.tsx
@@ -1,17 +1,69 @@
+import { Button, InputAdornment, Switch, TextField } from "@mui/material"
 import * as Styles from "./styles"
+import { useState } from "react"
+import  MailOutlineIcon from "@mui/icons-material/MailOutline"
+import KeyIcon from '@mui/icons-material/Key';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const SignInPage = () => {
     return (
-        <div id="SignInPage" style={Styles.SignInPageStyle}>
-
+        <div id="SignInPage" style={Styles.signInPageStyle}>
+            <EmailAndPasswordContainer/>
         </div>
     )
 }
 
 const EmailAndPasswordContainer = () => {
+    const [toggleButtonStatus,toggleButton] = useState(false)
+    const [isMouseHovered,setIsHovered] = useState(false)
+    let additionalToggleButtonStyle = {};
+    let toggleButtonBorderStyle = {};
+    let toggleButtonText = "Sign In"
+    if (toggleButtonStatus) {
+        additionalToggleButtonStyle = Styles.signOutToggleStyle
+        toggleButtonText = "Sign Up"
+    }
+    if (isMouseHovered) {
+        toggleButtonBorderStyle = Styles.toggleBorderStyle
+    }
     return (
-        <div>
-
+        <div id="EmailAndPasswordContainer" style={Styles.emailAndPasswordContainerStyle}>
+            <span style={Styles.firstHeaderStyle}>Welcome To PeerPrep!</span>
+            <span style={Styles.secondHeaderStyle}>Please enter your details</span>
+            <div style={Styles.switchStyle}>
+                <div id="SignInToggle" style={{...Styles.signInToggleStyle,
+                    ...additionalToggleButtonStyle,
+                    ...toggleButtonBorderStyle}} 
+                    onClick={() => toggleButton(!toggleButtonStatus)}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    <span style={Styles.toggleButtonTextStyle}>{toggleButtonText}</span>
+                </div>
+            </div>
+            <TextField label="username" sx={Styles.textFieldStyle} InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <MailOutlineIcon/>
+                    </InputAdornment>
+                )
+            }}>
+            </TextField>
+            <TextField label="password" sx={Styles.textFieldStyle} InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <KeyIcon/>
+                    </InputAdornment>
+                ),
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <VisibilityOffIcon/>
+                    </InputAdornment>
+                )
+            }}>
+            </TextField>
+            <Button variant="contained" sx={Styles.continueButtonStyle}>Continue</Button>
         </div>
     )
 }
--- front-end/peer-prep/src/components/Auth/SignInPage/styles.tsx
+++ front-end/peer-prep/src/components/Auth/SignInPage/styles.tsx
@@ -1,9 +1,115 @@
 
-export const SignInPageStyle = {
+export const signInPageStyle = {
     height: "100vh",
     width: "100%",
-    border: "2px solid green",
     display: "grid",
     gridTemplateColumns: "50% 50%",
     gridTemplateRows: "100%"
+}
+
+export const emailAndPasswordContainerStyle = {
+    height: "60%",
+    width: "60%",
+    gridColumn: "1 / 2",
+    gridRow: "1 / 1",
+    border: "2px solid blue",
+    justifySelf: "center",
+    alignSelf: "center",
+    display: "grid",
+    //justifyContent: "center",
+    justifyItems: "center",
+    //alignItems: "center",
+    alignContent: "center"
+}
+
+export const firstHeaderStyle = {
+    height: "50px",
+    paddingBottom: "20px",
+    //border: "2px solid red",
+    fontSize: "50px",
+    fontFamily: "arial"
+}
+
+export const secondHeaderStyle = {
+    //border: "2px solid red",
+    fontSize: "20px",
+    fontFamily: "arial",
+    paddingBottom: "20px"
+}
+
+export const switchStyle = {
+    height: "60px",
+    width: "500px",
+    //border: "1.5px solid black",
+    backgroundColor: "#1B2735",
+    borderRadius: "20px",
+    display: "flex",
+    flexDirection: "row" as "row",
+    alignItems: "center"
+}
+
+export const signInToggleStyle = {
+    backgroundColor: "white",
+    height: "100%",
+    width: "55%",
+    borderRadius: "20px",
+    display: "flex",
+    flexDirection: "row" as "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    border: "2px solid black"
+}
+
+export const signOutToggleStyle = {
+    //alignSelf: "flex-end"
+    marginLeft: "50%"
+}
+
+export const toggleBorderStyle = {
+    border: "2px solid pink",
+    color: "pink"
+}
+
+export const toggleButtonTextStyle = {
+    fontSize: "25px",
+    height: "30px",
+    fontFamily: "arial",
+}
+
+export const textFieldStyle = {
+    marginTop: "20px",
+    width: "500px",
+    '& label.Mui-focused': {
+        color: '#F4C2C2',
+    },
+	'& .MuiInput-underline:after': {
+		borderBottomColor: '#F4C2C2',
+	},
+	'& .MuiOutlinedInput-root': {
+        height: "60px",
+		'& fieldset': {
+			borderColor: "#1B2735",
+			borderRadius: "10px",
+		},
+		'&:hover fieldset': {
+			borderColor: '#F4C2C2',
+		},
+		'&.Mui-focused fieldset': {
+			borderColor: '#F4C2C2',
+		}
+    },
+}
+
+export const continueButtonStyle = {
+    backgroundColor: "#1B2735",
+    borderRadius: "15px",
+    marginTop: "20px",
+    height: "60px",
+    width: "500px",
+    '&:hover': {
+        backgroundColor: 'pink', // Change background color to pink on hover
+    },
+    fontSize: "20px"
 }
