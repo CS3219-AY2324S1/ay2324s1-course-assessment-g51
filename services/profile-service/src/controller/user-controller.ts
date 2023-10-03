8534666d45414b8f205e24fa4c6dbd40768542c7
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-10-03 14:27:22 +0800

    Add checks for duplicate email

--- services/profile-service/src/controller/user-controller.ts
+++ services/profile-service/src/controller/user-controller.ts
@@ -54,6 +54,15 @@ export class UserController {
 		if (!user) {
 			return ResponseUtil.sendError(res, "user not found", 404);
 		}
+
+		//check for duplicate email
+		const checkEmail = await AppDataSource.getRepository(User).findOneBy({
+			email: userData.email,
+		});
+		if (checkEmail) {
+			return ResponseUtil.sendError(res, "Email already used", 404);
+		}
+
 		repo.merge(user, userData);
 		await repo.save(user);
 		return ResponseUtil.sendResponse(res, user, 201);
