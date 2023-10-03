b61104aac0499568ee54c513c9e0abf459060ed7
Haiqel <34855234+Acerizm@users.noreply.github.com>
2023-10-03 19:41:42 +0800

    Create base authentication service (#25)
    * add firebase configs
    
    * Add styling to Sign In component
    
    * Add PeerPrep mian image
    
    * Update routing to include protected routes
    
    * Add login with email and password feature
    
    * Update error text for wrong email and password entered
    
    * Add create user with email and password feature
    
    * Remove visibility icon from second password text field
    
    * Remove print statements
    
    * Update indentation

---
+++ front-end/peer-prep/src/components/Auth/SignInPage/styles.tsx
@@ -0,0 +1,144 @@
+
+export const signInPageStyle = {
+    height: "100vh",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "50% 50%",
+    gridTemplateRows: "100%",
+    backgroundColor: "#131B25"
+}
+
+export const emailAndPasswordContainerStyle = {
+    height: "700px",
+    width: "700px",
+    gridColumn: "1 / 2",
+    gridRow: "1 / 1",
+    borderRadius: "20px",
+    justifySelf: "center",
+    alignSelf: "center",
+    display: "grid",
+    //justifyContent: "center",
+    justifyItems: "center",
+    //alignItems: "center",
+    alignContent: "center",
+    backgroundColor: "white",
+}
+
+export const firstHeaderStyle = {
+    height: "50px",
+    paddingBottom: "20px",
+    //border: "2px solid red",
+    fontSize: "50px",
+    fontFamily: "arial",
+    //color: "white"
+}
+
+export const secondHeaderStyle = {
+    //border: "2px solid red",
+    fontSize: "20px",
+    fontFamily: "arial",
+    paddingBottom: "20px",
+    //color: "white"
+}
+
+export const switchStyle = {
+    height: "60px",
+    width: "500px",
+    //border: "1.5px solid black",
+    backgroundColor: "#1B2735",
+    borderRadius: "20px",
+    display: "flex",
+    flexDirection: "row" as "row",
+    alignItems: "center"
+}
+
+export const signInToggleStyle = {
+    backgroundColor: "white",
+    height: "100%",
+    width: "55%",
+    borderRadius: "20px",
+    display: "flex",
+    flexDirection: "row" as "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    border: "2px solid black"
+}
+
+export const signOutToggleStyle = {
+    //alignSelf: "flex-end"
+    marginLeft: "50%"
+}
+
+export const toggleBorderStyle = {
+    border: "2px solid pink",
+    color: "pink"
+}
+
+export const toggleButtonTextStyle = {
+    fontSize: "25px",
+    height: "30px",
+    fontFamily: "arial",
+}
+
+export const textFieldStyle = {
+    marginTop: "20px",
+    width: "500px",
+    '& label.Mui-focused': {
+        color: '#F4C2C2',
+    },
+	'& .MuiInput-underline:after': {
+		borderBottomColor: '#F4C2C2',
+	},
+	'& .MuiOutlinedInput-root': {
+        height: "60px",
+		'& fieldset': {
+			borderColor: "#1B2735",
+			borderRadius: "10px",
+		},
+		'&:hover fieldset': {
+			borderColor: '#F4C2C2',
+		},
+		'&.Mui-focused fieldset': {
+			borderColor: '#F4C2C2',
+		}
+    },
+}
+
+export const continueButtonStyle = {
+    backgroundColor: "#1B2735",
+    borderRadius: "15px",
+    marginTop: "20px",
+    height: "60px",
+    width: "500px",
+    '&:hover': {
+        backgroundColor: 'pink', // Change background color to pink on hover
+    },
+    fontSize: "20px"
+}
+
+export const peerPrepImageContainerStyle = {
+    gridColumn: " 2 / 2",
+    gridRow: "1 / 1",
+    position: "relative" as "relative",
+    paddingTop: '56.25%', // 16:9 aspect ratio
+}
+
+export const imageStyle = {
+    position: "absolute" as "absolute",
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    objectFit: "cover" as "cover", // Ensure the image covers the entire container
+}
+
+export const errorTextStyle = {
+    width: "500px",
+    marginTop: "20px",
+    fontSize: "15px",
+    color: "red",
+    fontFamily: "arial",
+    textAlign: "center" as "center"
+}
