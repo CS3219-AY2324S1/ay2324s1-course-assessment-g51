015fb4887f2f83546333c895aef8bb76141b6e6e
Haiqel <34855234+Acerizm@users.noreply.github.com>
2023-10-05 11:20:55 +0800

    Update navbar

--- front-end/peer-prep/src/components/Navbar/index.tsx
+++ front-end/peer-prep/src/components/Navbar/index.tsx
@@ -1,13 +1,43 @@
 import AppBar from "@mui/material/AppBar"
+import Toolbar from '@mui/material/Toolbar';
 import * as Styles from "./styles"
+import Avatar from '@mui/material/Avatar';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+const navbarRoutes = [
+    {
+        text: "Question",
+        link: "/home"
+    },
+    {
+        text: "Profile",
+        link: "/user"
+    }
+]
 
 const Navbar = () => {
-
+    const routes = navbarRoutes;
+    const navigate = useNavigate();
     return (
-        <AppBar position="absolute" style={Styles.NavbarContainerStyle}>
-            
+        <AppBar position="absolute" style={Styles.navbarContainerStyle}>
+            <Toolbar>
+                <Box sx={Styles.navbarButtonContainerStyle}>
+                    {routes.map((item) => (
+                    <Button key={item.link} sx={Styles.navbarButtonStyle} onClick={(e) => {
+                        e.stopPropagation();
+                        navigate(item.link)
+                    }}>
+                        {item.text.toLowerCase()}
+                    </Button>
+                    ))}
+                </Box>
+                <Avatar sx={Styles.userNavigationAvatarStyle}/>
+            </Toolbar>
         </AppBar>
     )
 }
+
 
 export default Navbar
--- front-end/peer-prep/src/components/Navbar/styles.tsx
+++ front-end/peer-prep/src/components/Navbar/styles.tsx
@@ -1,5 +1,29 @@
-export const NavbarContainerStyle = {
-    //border: "2px solid red",
+export const navbarContainerStyle = {
     height: "60px",
-    width: "100%"
+    width: "100%",
+    borderBottom: "solid pink",
+    backgroundColor: "#132735"
+}
+
+export const userNavigationAvatarStyle = {
+    height: "40px",
+    width: "40px",
+    marginLeft:"auto",
+    marginRight: "50px"
+}
+
+export const navbarButtonContainerStyle = {
+    //border: "2px solid red",
+    width: "80%",
+    display: "flex",
+    marginRight: "100px",
+    flexDirection: "row" as "row",
+    justifyContent: "flex-end"
+}
+
+export const navbarButtonStyle = {
+    color: '#fff',
+    marginRight: "50px",
+    textTransform: "capitalize",
+    fontSize: "20px"
 }
