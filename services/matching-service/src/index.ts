852d576c03f8229c48c34a96c956c4dfc7666561
Hui Zhuan <huizhuansam@hotmail.com>
2023-10-11 22:49:54 +0800

    initial commit

---
+++ services/matching-service/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(8000, () => {
+  console.log("server running at http://localhost:8000");
+});
