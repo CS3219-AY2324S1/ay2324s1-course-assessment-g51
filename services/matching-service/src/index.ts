065e6a4a73a7692d69234683da8e257090e9a21a
Hui Zhuan <huizhuansam@hotmail.com>
2023-10-12 20:07:42 +0800

    add client side amqplib

--- services/matching-service/src/index.ts
+++ services/matching-service/src/index.ts
@@ -1,19 +1,52 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import amqplib from "amqplib";
+import { randomUUID } from "crypto";
+import Joi from "joi";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const matchRequestSchema = Joi.object({
+  userId: Joi.string().required(),
+  complexity: Joi.string().required().valid("easy", "medium", "difficult"),
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on("match-request:create", () => {});
-  socket.on("match-request:cancel", () => {});
+
+  socket.on("match-request:create", async (data) => {
+    try {
+      const matchRequest = matchRequestSchema.validate(data);
+      const amqpConnection = await amqplib.connect("amqp://localhost");
+      const channel = await amqpConnection.createChannel();
+      // Anonymous, exclusive callback queue. 1 callback queue per client.
+      const callbackQueue = await channel.assertQueue("", {
+        exclusive: true,
+      });
+      const correlationId = randomUUID();
+      const matchRequestQueue = "match-request-queue";
+      channel.sendToQueue(
+        matchRequestQueue,
+        Buffer.from(JSON.stringify(matchRequest)),
+        { correlationId, replyTo: callbackQueue.queue }
+      );
+      channel.consume(
+        callbackQueue.queue,
+        (msg) => {
+          if (msg?.properties.correlationId === correlationId) {
+            socket.emit("match-response:success", msg.content.toString());
+          }
+        },
+        { noAck: true }
+      );
+    } catch (err) {
+      socket.emit("match-response:error", err);
+    }
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
@@ -26,3 +59,7 @@ app.all("*", (_, res) => {
 server.listen(8000, () => {
   console.log("server running at http://localhost:8000");
 });
+
+// Client -> API -> Queue -> Worker
+// Detect state change in db
+// API -> Client
