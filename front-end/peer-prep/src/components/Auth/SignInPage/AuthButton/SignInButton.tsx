b30d8b44957a32f0c0cb1a293aa09c39b0265253
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-10-21 03:30:58 +0800

    change http to https

--- front-end/peer-prep/src/components/Auth/SignInPage/AuthButton/SignInButton.tsx
+++ front-end/peer-prep/src/components/Auth/SignInPage/AuthButton/SignInButton.tsx
@@ -1,59 +1,79 @@
 import { IconButton } from "@mui/material";
 
-import * as Styles from "../styles"
+import * as Styles from "../styles";
 
 import { useNavigate } from "react-router-dom";
 
 import { auth } from "../../Firebase";
-import { GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider, TwitterAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+	GoogleAuthProvider,
+	FacebookAuthProvider,
+	GithubAuthProvider,
+	TwitterAuthProvider,
+	signInWithPopup,
+} from "firebase/auth";
 
 import { useDispatch, useSelector } from "react-redux";
-import * as UserSlice from "../../../redux/reducers/User/UserSlice"
+import * as UserSlice from "../../../redux/reducers/User/UserSlice";
 
 import axios from "axios";
 
 interface SignInButtonProps {
-    provider: GoogleAuthProvider | FacebookAuthProvider | GithubAuthProvider | TwitterAuthProvider;
-    iconImage: string;
-    iconImageAlt: string;
+	provider:
+		| GoogleAuthProvider
+		| FacebookAuthProvider
+		| GithubAuthProvider
+		| TwitterAuthProvider;
+	iconImage: string;
+	iconImageAlt: string;
 }
 
-const SignInButton = ({ provider, iconImage, iconImageAlt }: SignInButtonProps) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const isNewUser = useSelector(UserSlice.selectIsFirstTimeLogin);
+const SignInButton = ({
+	provider,
+	iconImage,
+	iconImageAlt,
+}: SignInButtonProps) => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const isNewUser = useSelector(UserSlice.selectIsFirstTimeLogin);
 
-    provider.addScope('email');
+	provider.addScope("email");
 
-    const handleSignIn = () => signInWithPopup(auth, provider)
-        .then((result) => {
-            // The signed-in user info
-            const user = result.user;
-            const uid = user.uid;
+	const handleSignIn = () =>
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				// The signed-in user info
+				const user = result.user;
+				const uid = user.uid;
 
-            // Checks if user is new.
-            axios({
-                method: 'get',
-                url: `http://api.peerprepgroup51sem1y2023.xyz/users/${uid}`
-            }).catch((error) => {
-                console.log(error)
-                dispatch(UserSlice.setIsFirstTimeLogin(true))
-            })
+				// Checks if user is new.
+				axios({
+					method: "get",
+					url: `https://api.peerprepgroup51sem1y2023.xyz/users/${uid}`,
+				}).catch((error) => {
+					console.log(error);
+					dispatch(UserSlice.setIsFirstTimeLogin(true));
+				});
 
-            if (isNewUser) {
-                navigate("/user");
-            } else {
-                navigate("/home");
-            }
-        }).catch((error) => {
-            console.log(error)
-        });
+				if (isNewUser) {
+					navigate("/user");
+				} else {
+					navigate("/home");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 
-    return (
-        <IconButton onClick={() => { handleSignIn() }}>
-            <img style={Styles.iconStyle} src={iconImage} alt={iconImageAlt} />
-        </IconButton>
-    )
-}
+	return (
+		<IconButton
+			onClick={() => {
+				handleSignIn();
+			}}
+		>
+			<img style={Styles.iconStyle} src={iconImage} alt={iconImageAlt} />
+		</IconButton>
+	);
+};
 
 export default SignInButton;
