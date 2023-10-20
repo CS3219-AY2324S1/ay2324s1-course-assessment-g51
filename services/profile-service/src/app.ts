d18902aaa050462596aaed53ac2a9aac85cd2e41
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-10-21 02:31:17 +0800

    Update cors policy

--- services/profile-service/src/app.ts
+++ services/profile-service/src/app.ts
@@ -1,8 +1,9 @@
 import bodyParser from "body-parser";
-import cors from "cors";
 import express, { Express, NextFunction, Request, Response } from "express";
 import "reflect-metadata";
 import userRoutes from "./router/user-routes";
+
+var cors = require("cors");
 
 const app = express();
 
@@ -30,7 +31,28 @@ const app = express();
 
 // app.use(cors(corsOptions));
 
-app.use(cors());
+var allowedOrigins = [
+	"http://localhost:3000",
+	"https://app.peerprepgroup51sem1y2023.xyz",
+];
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				var msg =
+					"The CORS policy for this site does not " +
+					"allow access from the specified Origin.";
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
+
+//app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/users", userRoutes);
