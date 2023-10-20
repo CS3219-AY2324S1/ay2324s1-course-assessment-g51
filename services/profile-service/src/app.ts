55c50cf802236541a119f22f8ead749971c25079
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-10-21 02:42:55 +0800

    Update cors policy

--- services/profile-service/src/app.ts
+++ services/profile-service/src/app.ts
@@ -31,24 +31,9 @@ const app = express();
 
 // app.use(cors(corsOptions));
 
-var allowedOrigins = [
-	"http://localhost:3000",
-	"https://app.peerprepgroup51sem1y2023.xyz",
-];
 app.use(
 	cors({
-		origin: function (origin, callback) {
-			// allow requests with no origin
-			// (like mobile apps or curl requests)
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				var msg =
-					"The CORS policy for this site does not " +
-					"allow access from the specified Origin.";
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		},
+		origin: "*", // Allow requests from any origin
 	})
 );
 
