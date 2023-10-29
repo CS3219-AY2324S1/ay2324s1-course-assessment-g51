29e42d4241f9e63a40fb9ab099b214afbff7a29f
Hui Zhuan <huizhuansam@hotmail.com>
2023-10-29 17:34:54 +0800

    support matching by language

--- services/matching-service/src/utils/dataValidation.ts
+++ services/matching-service/src/utils/dataValidation.ts
@@ -3,6 +3,9 @@ import Joi from "joi";
 const matchRequestSchema = Joi.object({
   userId: Joi.string().required(),
   complexity: Joi.string().required().valid("easy", "medium", "difficult"),
+  languages: Joi.array()
+    .required()
+    .items(Joi.string().valid("python", "javascript", "c++", "java")),
 });
 
 const validateMatchRequestPromise = (data: any) =>
