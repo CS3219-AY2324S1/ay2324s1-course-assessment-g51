e6ee564ce2ad5dac3e2922c173194983972c1605
Zenith Yap <zenithyap@Zeniths-MacBook-Air.local>
2023-10-31 23:32:36 +0800

    Create UI for question view

--- front-end/peer-prep/src/components/PracticePage/QuestionView/styles.tsx
+++ front-end/peer-prep/src/components/PracticePage/QuestionView/styles.tsx
@@ -3,4 +3,38 @@ export const questionViewContainerStyle = {
     height: "80%",
     width: "90%",
     display: "grid",
+    borderRadius: "15px", 
+    alignItems: "center",
+    justifyItems: "center",
+}
+
+export const questionViewMainStyle = {
+    height: "80%",
+    width: "80%",
+    gridTemplateRows: "1fr 1fr 1fr 1fr 8fr",
+    display: "grid",
+}
+
+export const titleStyle = {
+    color: "white",
+    fontSize: "20px",
+}
+
+export const difficultyChipStyle = {
+	color: "#90EE90",
+	fontFamily: "arial",
+    backgroundColor: "#1B2735",
+	fontSize: "15px",
+};
+
+export const categoryChipStyle = {
+	color: "white",
+	fontFamily: "arial",
+    backgroundColor: "#1B2735",
+	fontSize: "15px",
+};
+
+export const descriptionStyle = {
+    color: "white",
+    gridRow: "5/5"
 }
