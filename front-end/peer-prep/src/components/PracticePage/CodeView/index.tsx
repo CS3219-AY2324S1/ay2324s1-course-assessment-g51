d7d1d39f9a36b4455e88dbccf58b3e8cab863f4d
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-11-06 21:37:29 +0800

    Add live text editor

--- front-end/peer-prep/src/components/PracticePage/CodeView/index.tsx
+++ front-end/peer-prep/src/components/PracticePage/CodeView/index.tsx
@@ -1,13 +1,34 @@
-import * as Styles from './styles';
-
-import Editor from '@monaco-editor/react';
+import { useEffect, useState } from "react";
+import * as Styles from "./styles";
+import { socket } from "../index";
+import Editor from "@monaco-editor/react";
 
 const CodeView = () => {
-    return (
-        <div style={Styles.CodeViewContainerStyle}>
-            <Editor defaultLanguage="javascript" defaultValue="//@Kang Quan all yours bro :)" />
-        </div>
-    )
-}
+	const [code, setCode] = useState("");
+
+	useEffect(() => {
+		// Listen for code changes from other clients and update the editor
+		socket.on("code-change", (newCode: string) => {
+			setCode(newCode);
+		});
+	}, [socket]);
+
+	const handleCodeChange = (newCode: string) => {
+		setCode(newCode);
+		// Send the new code to the server
+		socket.emit("code-change", newCode);
+	};
+	return (
+		<div style={Styles.CodeViewContainerStyle}>
+			<Editor
+				defaultLanguage="javascript"
+				defaultValue="//@Kang Quan all yours bro :)"
+				language="javascript"
+				value={code}
+				onChange={() => handleCodeChange(code)}
+			/>
+		</div>
+	);
+};
 
 export default CodeView;
