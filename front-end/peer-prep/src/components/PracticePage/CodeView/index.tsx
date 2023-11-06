a21dda6f464973aee688525f83ad20e261a6ba54
Kang-Quan <105635942+Kang-Quan@users.noreply.github.com>
2023-11-06 23:30:25 +0800

    Add texteditor socketio (#104)
    * Add live text editor
    
    * update collab service
    
    * Add code sharing of editor to codebase
    
    * Add judge0 compiler
    
    * Change localhost to actual url

--- front-end/peer-prep/src/components/PracticePage/CodeView/index.tsx
+++ front-end/peer-prep/src/components/PracticePage/CodeView/index.tsx
@@ -1,13 +1,61 @@
-import * as Styles from './styles';
+import { useEffect, useState } from "react";
+import * as Styles from "./styles";
+import { socket } from "../index";
+import Editor from "@monaco-editor/react";
+import { useSelector } from "react-redux";
+import * as MatchSlice from "../../redux/reducers/Match/MatchSlice";
 
-import Editor from '@monaco-editor/react';
+interface IPartnerDetails {
+	userId1: string;
+	userId2: string;
+	complexity: string;
+	matchId: string;
+	language: string;
+}
+
+interface IMessageData {
+	message: string;
+	roomId: string;
+	socketId: string;
+	isMine: boolean;
+}
 
 const CodeView = () => {
-    return (
-        <div style={Styles.CodeViewContainerStyle}>
-            <Editor defaultLanguage="javascript" defaultValue="//@Kang Quan all yours bro :)" />
-        </div>
-    )
-}
+	const [code, setCode] = useState("");
+	const partnerDetails: IPartnerDetails = useSelector(
+		MatchSlice.selectPartnerDetails
+	);
+	const roomId = partnerDetails.matchId;
+
+	useEffect(() => {
+		// Listen for code changes from other clients and update the editor
+		socket.on("code-change", (data: IMessageData) => {
+			console.log(data.message);
+			setCode(data.message);
+		});
+	}, [socket]);
+
+	const handleCodeChange = (newCode: any, event: any) => {
+		setCode(newCode);
+		// Send the new code to the server
+		socket.emit("code-change", {
+			message: newCode,
+			roomId: roomId,
+			socketId: socket.id,
+			isMine: true,
+		});
+	};
+	return (
+		<div style={Styles.CodeViewContainerStyle}>
+			<Editor
+				defaultLanguage="javascript"
+				defaultValue="//@Kang Quan all yours bro :)"
+				language="javascript"
+				value={code}
+				onChange={handleCodeChange}
+			/>
+		</div>
+	);
+};
 
 export default CodeView;
--- services/collaboration-service/index.ts
+++ services/collaboration-service/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import http from 'http';
-import { Server as socketIo } from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server as socketIo } from "socket.io";
 import cors from "cors";
 
 interface IMessageData {
-    message: string,
-    roomId: string,
-    socketId: string,
-    isMine: boolean
-};
+	message: string;
+	roomId: string;
+	socketId: string;
+	isMine: boolean;
+}
 
 const app = express();
 app.use(
@@ -21,57 +21,89 @@ const io = new socketIo(server, {});
 
 const clientsByRoom = new Map();
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+io.on("connection", (socket) => {
+	console.log("A user connected");
 
-    let userRoom = null;
+	let userRoom = null;
 
-    // When a client connects, ask them to specify a room to join
-    socket.on('joinRoom', (room) => {
-        socket.join(room);
-        userRoom = room;
+	// When a client connects, ask them to specify a room to join
+	socket.on("joinRoom", (room) => {
+		socket.join(room);
+		userRoom = room;
 
-        // Store the socket in the room's client list
-        if (!clientsByRoom.has(room)) {
-        clientsByRoom.set(room, new Set());
-        }
-        clientsByRoom.get(room).add(socket);
+		// Store the socket in the room's client list
+		if (!clientsByRoom.has(room)) {
+			clientsByRoom.set(room, new Set());
+		}
+		clientsByRoom.get(room).add(socket);
 
-        console.log(`User joined room: ${room}`);
-    });
+		console.log(`User joined room: ${room}`);
+	});
 
-    socket.on('message', (data:IMessageData) => {
-        console.log("test")
-        console.log('Received message:', data);
-        try {
-        // Broadcast the message to all connected clients
-            const room = data.roomId;
-            if (!room) {
-                socket.emit('error', 'An error occurred while processing your request');
-            }
-            // Emit the message to the specified room
-            io.to(room).emit('message', data);
-        } catch (error) {
-        // Handle the error
-        console.error('An error occurred in the event handler:', error);
-    
-        // You can emit an error event back to the client if needed
-        socket.emit('error', 'An error occurred while processing your request');
-        }
-    });
+	socket.on("message", (data: IMessageData) => {
+		console.log("test");
+		console.log("Received message:", data);
+		try {
+			// Broadcast the message to all connected clients
+			const room = data.roomId;
+			if (!room) {
+				socket.emit(
+					"error",
+					"An error occurred while processing your request"
+				);
+			}
+			// Emit the message to the specified room
+			io.to(room).emit("message", data);
+		} catch (error) {
+			// Handle the error
+			console.error("An error occurred in the event handler:", error);
 
-    socket.on('error', (error) => {
-        console.log(error);
-    });
+			// You can emit an error event back to the client if needed
+			socket.emit(
+				"error",
+				"An error occurred while processing your request"
+			);
+		}
+	});
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-        io.to(userRoom).emit('userDisconnect')
-    });
+	socket.on("error", (error) => {
+		console.log(error);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("A user disconnected");
+		io.to(userRoom).emit("userDisconnect");
+	});
+
+	socket.on("code-change", (data: IMessageData) => {
+		console.log("test");
+		console.log("Received message:", data);
+		try {
+			// Broadcast the message to all connected clients
+			const room = data.roomId;
+			if (!room) {
+				socket.emit(
+					"error",
+					"An error occurred while processing your request"
+				);
+			}
+			// Emit the message to the specified room
+			io.to(room).emit("code-change", data);
+		} catch (error) {
+			// Handle the error
+			console.error("An error occurred in the event handler:", error);
+
+			// You can emit an error event back to the client if needed
+			socket.emit(
+				"error",
+				"An error occurred while processing your request"
+			);
+		}
+	});
 });
 
 const port = process.env.PORT || 8576;
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
