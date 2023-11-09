85a37710e3d1c86e24b0a59e4798fbacedab4226
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-11-09 16:26:32 +0800

    Update styling of circular progress to ensure it is consistent with the color theme and size

--- front-end/peer-prep/src/components/MatchingServicePopUp/CustomCircularProgress.tsx
+++ front-end/peer-prep/src/components/MatchingServicePopUp/CustomCircularProgress.tsx
@@ -1,51 +1,57 @@
-import * as React from 'react';
+import * as React from "react";
 import CircularProgress, {
-  CircularProgressProps,
-} from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+	CircularProgressProps,
+} from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 function CircularProgressWithLabel(
-  props: CircularProgressProps & { value: number },
+	props: CircularProgressProps & { value: number }
 ) {
-  return (
-    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-      <CircularProgress variant="determinate" {...props} />
-      <Box
-        sx={{
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          position: 'absolute',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          color: 'pink',
-
-        }}
-      >
-        <Typography
-          variant="caption"
-          component="div"
-          color="white"
-        >{`${Math.floor(props.value / 3.33)}s`}</Typography>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box sx={{ position: "relative", display: "inline-flex" }}>
+			<CircularProgress
+				variant="determinate"
+				sx={{ color: "pink" }}
+				size={50}
+				{...props}
+			/>
+			<Box
+				sx={{
+					top: 0,
+					left: 0,
+					bottom: 0,
+					right: 0,
+					position: "absolute",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					color: "pink",
+				}}
+			>
+				<Typography
+					variant="caption"
+					component="div"
+					color="white"
+				>{`${Math.floor(props.value / 3.33)}s`}</Typography>
+			</Box>
+		</Box>
+	);
 }
 
 export default function CustomCircularProgress() {
-  const [progress, setProgress] = React.useState(100);
+	const [progress, setProgress] = React.useState(100);
 
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress > 0.2 ? prevProgress - 3.33 : 0));
-    }, 1000);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+	React.useEffect(() => {
+		const timer = setInterval(() => {
+			setProgress((prevProgress) =>
+				prevProgress > 0.2 ? prevProgress - 3.33 : 0
+			);
+		}, 1000);
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
 
-  return <CircularProgressWithLabel value={progress} />;
+	return <CircularProgressWithLabel value={progress} />;
 }
